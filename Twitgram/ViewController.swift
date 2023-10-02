09eb0620eec8e6a3b860d4864f54e4a17b8d5e73
Ernazar02A <119403073+Ernazar02A@users.noreply.github.com>
2023-10-02 18:21:32 +0600

    Initial Commit

---
+++ Twitgram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitgram
+//
+//  Created by Ernazar on 2/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
